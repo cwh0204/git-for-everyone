8e56d95858eb6115be9924cc64ab5d03fd83b4ed
cwh0204 <48270074+cwh0204@users.noreply.github.com>
2023-12-06 13:14:41 +0900

    add_Color
    add_Color

--- flutter_application_test/lib/main.dart
+++ flutter_application_test/lib/main.dart
@@ -30,13 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  Color colors = Colors.white;
 
   @override
   Widget build(BuildContext context) {
